f548743d509f79397b202bf6dcff7f1ba5c1139f
yhpark <1771360@hansung.ac.kr>
2021-03-19 15:58:56 +0900

    pom.xml 수정 및 PlaceDao.java 추가

---
+++ HJJH_Trip/src/dao/PlaceDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+public class PlaceDao {
+
+}
